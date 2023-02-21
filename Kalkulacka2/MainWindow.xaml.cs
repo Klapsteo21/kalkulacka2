3793a799077b6a75444f423ac2d457e41880b037
Ondřej Klápště <klapsteo21@spstrutnov.cz>
2023-02-21 10:58:30 +0100

    3 řady

--- Kalkulacka2/MainWindow.xaml.cs
+++ Kalkulacka2/MainWindow.xaml.cs
@@ -24,5 +24,10 @@ namespace Kalkulacka2
         {
             InitializeComponent();
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
